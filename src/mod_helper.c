86242a97168d0f88c1d4972e6d465a8219611073
jsjsjsjsjsjsjson <blocklu@163.com>
2026-05-24 07:54:09 +0800

    Add period to string helper

---
+++ src/mod_helper.c
@@ -0,0 +1,27 @@
+#include "mod_helper.h"
+#include <string.h>
+
+void mod_period_to_note_str(int period, char *buf) {
+    if (period > MOD_PERIOD_MAX) {
+        strcpy(buf, "LOW");
+        return;
+    }
+
+    if (period < MOD_PERIOD_MIN) {
+        strcpy(buf, "HIG");
+        return;
+    }
+
+    uint8_t ni = period_to_note_lut[period];
+    if (ni == 0) {
+        strcpy(buf, "UNK");
+        return;
+    }
+
+    const char *n = NOTE_CHARS[ni % 12];
+
+    buf[0] = n[0];
+    buf[1] = n[1];
+    buf[2] = '1' + (ni / 12);
+    buf[3] = '\0';
+}
